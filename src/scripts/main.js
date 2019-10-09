601500169a1116ee0992cf49f6cfeaea93d10019
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-09 14:18:18 -0500

    First exercise done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
-console.log("We're learning about objects!")
+// Lightning Exercise 1: Given two options of data structures - array or object - which should you use ? You need to represent a red 2015 Ford Mustang in your code.
+
+const car = {
+    year: 2015,
+    make: "Ford",
+    model: "Mustang",
+    color: "red",
+}
