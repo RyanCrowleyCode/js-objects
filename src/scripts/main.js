ecb0be5502488e0120881fe85104351d409b3d7d
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-09 16:06:33 -0500

    added question to main.js for lightning 3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -61,3 +61,22 @@ for (let i = 0; i < wardrobe.contents.length; i++) {
 
 console.log("Wardrobe depth", wardrobe.depth)
 console.log("Wardrobe width", wardrobe.width)
+
+// LIGHTNING 3!!!!
+const empireStateBuilding = {
+    stories: 103,
+    height: 1453,
+    address: "350 Fifth Avenue, Manhattan, New York 10118",
+    squareFeet: 2768591,
+    constructionDate: 1931,
+    cost: 40948900,
+    owner: "Empire State Realty Trust",
+    eastWestLength: 424,
+    northSouthLength: 187,
+    architect: "Shreve, Lamb & Harmon"
+}
+// Lightning Exercises 1: Use dot notation to output all of the dimensions of the Empire State Building to the console.
+
+
+
+// Lightning Exercises 2: Use square bracket notation to output the remaining 5 properties to the console. Create 5 variables first with the keys as their values. Use those variables to look up the values.
