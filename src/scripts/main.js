8bf0a53d8481874bc83d42f25e05daae5b586421
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-09 14:21:59 -0500

    Exercise 2 is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,7 @@ const car = {
     model: "Mustang",
     color: "red",
 }
+
+// Lightning Exercise 2: Given two options of data structures - array or object - which should you use? You need to store list of animal names in a shelter. The names are "Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", and "Sharky".
+
+const shelterAnimals = ["Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky"]
