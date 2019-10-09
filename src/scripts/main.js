8892d4c65a3bb0eb2c6809b5757f5df1a53bf73f
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-09 16:16:48 -0500

    lightning exercise 3 done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -77,6 +77,23 @@ const empireStateBuilding = {
 }
 // Lightning Exercises 1: Use dot notation to output all of the dimensions of the Empire State Building to the console.
 
+console.log(`Empire State Building stories: ${empireStateBuilding.stories}`)
+console.log(`Empire State Building height: ${empireStateBuilding.height}`)
+console.log(`Empire State Building square feet: ${empireStateBuilding.squareFeet}`)
+console.log(`Empire State Building east to west length: ${empireStateBuilding.eastWestLength}`)
+console.log(`Empire State Building north to south length: ${empireStateBuilding.northSouthLength}`)
 
 
 // Lightning Exercises 2: Use square bracket notation to output the remaining 5 properties to the console. Create 5 variables first with the keys as their values. Use those variables to look up the values.
+
+const empireAddress = "address"
+const empireConstructionDate = "constructionDate"
+const empireCost = "cost"
+const empireOwner = "owner"
+const empireArchitect = "architect"
+
+console.log(`Empire State Building address: ${empireStateBuilding[empireAddress]}`)
+console.log(`Empire State Building construction date: ${empireStateBuilding[empireConstructionDate]}`)
+console.log(`Empire State Building cost: ${empireStateBuilding[empireCost]}`)
+console.log(`Empire State Building owner: ${empireStateBuilding[empireAddress]}`)
+console.log(`Empire State Building architect: ${empireStateBuilding[empireArchitect]}`)
